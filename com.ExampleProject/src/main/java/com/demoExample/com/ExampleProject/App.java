85d81fee2d2a289d3a3b63e3c882809413f78cce
nomere <nomere@sapient.com>
2018-04-06 16:57:55 +0530

    adding project to master

---
+++ com.ExampleProject/src/main/java/com/demoExample/com/ExampleProject/App.java
@@ -0,0 +1,13 @@
+package com.demoExample.com.ExampleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
